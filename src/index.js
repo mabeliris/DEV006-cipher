4fb60d1e43f424b46ab7ffbc56a1fd39e3ed21da
mabeliris <mabelle7623@gmail.com>
2023-03-30 14:00:25 -0300

    proyecto terminado

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+document.getElementById("botonC").addEventListener("click",cipher.encode);
+document.getElementById("botonD").addEventListener("click",cipher.decode);
+
+
+
+/*llamar el metodo cipher
+cipher.encode()*/
+
+
+
+
+
+
+
